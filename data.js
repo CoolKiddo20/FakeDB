b1c52fb6ffaa96c7ece38b5165b0c2af73bcbd7a
CoolKiddo20 <161250160+CoolKiddo20@users.noreply.github.com>
2025-08-16 00:14:55 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+// data.js
+
+window.data = {
+  users: [
+    { id: 1, name: "John Doe", email: "johndoe@example.com", age: 28, country: "USA", isActive: true },
+    { id: 2, name: "Aisha Khan", email: "aisha.khan@example.com", age: 24, country: "Pakistan", isActive: false },
+    { id: 3, name: "Akmal Bek", email: "akmal.bek@example.com", age: 31, country: "Uzbekistan", isActive: true },
+    { id: 4, name: "Maria Garcia", email: "maria.garcia@example.com", age: 26, country: "Spain", isActive: true },
+    { id: 5, name: "Liam Smith", email: "liam.smith@example.com", age: 29, country: "UK", isActive: false }
+  ]
+};
